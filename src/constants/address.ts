1000d5bd210cc200d8c7132e66f077d7f2a2b67a
Neelansh Mathur <53081208+neelansh15@users.noreply.github.com>
2025-09-28 07:09:04 +0530

    chore: add hosted BE url

--- src/constants/address.ts
+++ src/constants/address.ts
@@ -5,6 +5,9 @@ export const ONE_INCH_AGGREGATOR_ADDRESS = {
   [bsc.id]: "",
 };
 
-export const OPERATOR_ADDRESS = "0x3230B799d1E7dDE6cF1B97Cd8AE991EfAe705e41"
+export const OPERATOR_ADDRESS = "0x3230B799d1E7dDE6cF1B97Cd8AE991EfAe705e41";
 
-export const API = "http://localhost:8008"
+export const API =
+  process.env.NODE_ENV == "development"
+    ? "http://localhost:8008"
+    : "https://goldengate-service.onrender.com";
