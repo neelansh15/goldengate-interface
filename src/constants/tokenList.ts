85b6da494d05c6c3c8822bb4065b7b04ca13bf53
Neelansh Mathur <53081208+neelansh15@users.noreply.github.com>
2025-09-28 07:58:47 +0530

    feat: add polygon tokenlist

--- src/constants/tokenList.ts
+++ src/constants/tokenList.ts
@@ -1,8 +1,10 @@
-import { bsc, mainnet } from "viem/chains";
+import { bsc, mainnet, polygon } from "viem/chains";
 import bcsTokenList from "./tokenLists/bscTokenList.json";
 import ethTokenList from "./tokenLists/ethTokenList.json";
+import polygonTokenList from "./tokenLists/polygonTokenList.json";
 
 export const tokenList = {
   [bsc.id]: bcsTokenList.tokens,
   [mainnet.id]: ethTokenList.tokens,
+  [polygon.id]: polygonTokenList.tokens,
 };
